d34f81a6a637463a8f61faa423ecf024329c6e18
kiransnambiar001 <kiransnambiar001@gmail.com>
2025-03-31 19:32:50 -0500

    Updated control panel
    updated video call control panel to have icons instead of text

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'});
 let localVideoTrack;
 let localMicTrack;
 let remoteUsers = {};
+let micMuted = false;
+let cameraMuted = false;
 
 
 
@@ -68,7 +70,8 @@ let joinAndDisplayLocalStream = async () => {
             await client.join(APP_ID, CHANNEL, TOKEN, uid+username);
 
             // Convert the local preview tracks to AgoraRTC tracks
-            localVideoTrack = await AgoraRTC.createCustomVideoTrack({ mediaStreamTrack: localStream.getVideoTracks()[0] });
+            try {localVideoTrack = await AgoraRTC.createCustomVideoTrack({ mediaStreamTrack: localStream.getVideoTracks()[0] });}
+            catch {}
             localMicTrack = await AgoraRTC.createCustomAudioTrack({ mediaStreamTrack: localStream.getAudioTracks()[0] });
 
             // Publish the same video preview to Agora
@@ -127,7 +130,9 @@ let handleUserJoined = async (user, mediaType) => {
     }
 
     // show user joined toast
-    let toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('userJoinedToast'));
+    let toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('top-right-toast'));
+    document.getElementById('top-right-toast-text').innerHTML = `User ${username}`
+    document.getElementById('top-right-toast-title').innerHTML = `User Joined`
     toastBootstrap.show()
     document.getElementById('emptyCallText').style.display = 'none';
 
@@ -226,31 +231,41 @@ let leaveStreamAndRemoveLocalStream = async() => {
 }
 
 let toggleMic = async (event) => {
-    let micButton = event.target
+    let micButton = document.getElementById('mic-btn');
     if (localMicTrack.muted) {
         await localMicTrack.setMuted(false);
-        micButton.innerHTML = 'Mic On';
-        micButton.className = 'btn btn-secondary';
+        micButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16" style="color: white;">
+            <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/>
+            <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
+          </svg>`;
+        micButton.className = 'btn btn-secondary rounded-pill shadow';
     } else {
         await localMicTrack.setMuted(true)
-        micButton.innerHTML = 'Mic Off';
-        micButton.className = 'btn btn-danger';
+        micButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16" style="color:white;">
+  <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4 4 0 0 0 12 8V7a.5.5 0 0 1 1 0zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a5 5 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4m3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3"/>
+  <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607m-7.84-9.253 12 12 .708-.708-12-12z"/>
+</svg>`;
+        micButton.className = 'btn btn-danger rounded-pill shadow';
     }
 }
 
 let toggleCam = async (event) => {
-    let camButton = event.target;
+    let camButton = document.getElementById('cam-btn');
     let localVideoElement = document.getElementById("local-preview");
 
     if (localVideoElement) {
         if (localVideoElement.style.display === "none") {
             localVideoElement.style.display = "block";
-            camButton.innerHTML = 'Camera On';
-            camButton.className = 'btn btn-secondary';
+            camButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16" style="color: white;">
+            <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2z"/>
+          </svg>`;
+            camButton.className = 'btn btn-secondary rounded-pill shadow';
         } else {
             localVideoElement.style.display = "none";
-            camButton.innerHTML = 'Camera Off';
-            camButton.className = 'btn btn-danger';
+            camButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-camera-video-off-fill" viewBox="0 0 16 16" style="color:white;">
+  <path fill-rule="evenodd" d="M10.961 12.365a2 2 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272zm-10.114-9A2 2 0 0 0 0 5v6a2 2 0 0 0 2 2h5.728zm9.746 11.925-10-14 .814-.58 10 14z"/>
+</svg>`;
+            camButton.className = 'btn btn-danger rounded-pill shadow';
         }
     }
 
