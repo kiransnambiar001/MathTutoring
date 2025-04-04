b2f395478e6e8ae1294890ad3c1f24a5ffff409d
kiransnambiar001 <kiransnambiar001@gmail.com>
2025-04-03 19:35:24 -0500

    started on whiteboard theorem pythagorana
    fdsafdsafdsafdsafdsaf

---
+++ whiteboard/whiteBoard.js
@@ -0,0 +1,19 @@
+// fetch("https://api.netless.link/v5/rooms", {
+//   method: "POST",
+//   headers: {
+//     "token": "NETLESSSDK_YWs9NTdJaVRwVENRYm1aUlBGTyZub25jZT1jOWQ0Y2RjMC0xMGU0LTExZjAtOWFmYi00M2Q0MzY5NGU2MWImcm9sZT0wJnNpZz1mNDk2NDRkYTNmMGVkNjc5NzNlMTNlYzg5NTNhMTYzZDE5YjMxODdiNmYxOGZiMjExYWRiNDNiYTJhOGNiNTY3",
+//     "Content-Type": "application/json",
+//     "region": "us-sv"
+//   },
+//   body: JSON.stringify({
+//     isRecord: false
+//   })
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log("data here:")
+//   console.log(data);
+// })
+// .catch(error => {
+//   console.error("Error with creating whiteboard room:", error);
+// });
